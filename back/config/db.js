9ede29da104653c632e9c88058776a41b2ea2744
user-ben-001 <mantrand.benjamin@gmail.com>
2026-03-12 16:33:36 +0100

    stop

--- back/config/db.js
+++ back/config/db.js
@@ -7,6 +7,7 @@ const db = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false },
 });
 
 try {
