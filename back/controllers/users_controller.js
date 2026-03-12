68336aaa2673f15307ff57c4e0cd2e7f43508ede
user-ben-001 <mantrand.benjamin@gmail.com>
2026-03-12 14:07:15 +0100

    fix

--- back/controllers/users_controller.js
+++ back/controllers/users_controller.js
@@ -74,7 +74,7 @@ export const login_controller = async (req, res) => {
       });
     });
   } catch (error) {
-    return res.status(500).json({ error: error, request: req });
+    return res.status(500).json({ error: error, request: req.body });
   }
 };
 
